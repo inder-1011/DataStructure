25899e6f6e93f439e39188c0aede571e71f584c8
indrajeet <indroojeet91@gmail.com>
2024-02-10 10:40:22 +0530

    files added

---
+++ DataStructureAndAlgorithms/src/main/java/com/example/prcatice/DataStructure/Array/AlternateCondition.java
@@ -0,0 +1,12 @@
+package com.example.prcatice.DataStructure.Array;
+
+public class AlternateCondition {
+
+   /* public static void main(String[] args) {
+        int[] arr= { 1, 3, 2, 2, 5 };
+        int[] result= rearrange(arr);
+    }
+
+    private static int[] rearrange(int[] arr) {
+    }*/
+}
